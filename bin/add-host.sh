e5772e9c1bbc731b1e4fe803fec9d339c06ae1fb
Andrew Cutler <macropin@gmail.com>
2012-10-14 23:25:58 +1100

    Additional commands

--- bin/add-host.sh
+++ bin/add-host.sh
@@ -8,11 +8,16 @@ if [ ! $HOST ]; then
 fi
 
 
+# setup backup zpool, set compression, set dedupe and inherit on backup/hosts 
 # zfs create backup/hosts
+# zfs set compression=gzip backup
+# zfs set dedup=on backup
+# zfs inherit -r compression backup/hosts
+# zfs inherit -r dedup backup/hosts
+
 # zfs create backup/hosts/example.com
 # zfs snapshot backup/hosts/example.com@foo
 # zfs destroy backup/hosts/example.com@foo
-
 
 # zfs create backup/hosts/vz01.in.adlibre.net
 # mkdir hosts/vz01.in.adlibre.net/c
@@ -21,4 +26,3 @@ fi
 # cp etc/host_default.conf hosts/vz01.in.adlibre.net/c/backup.conf
 
 
-# need to setup backup zpool, set compression, set dedupe and inherit on backup/hosts 
