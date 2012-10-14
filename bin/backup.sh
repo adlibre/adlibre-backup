1f9ca26aa52e77abb4e214778ee36184b97f21f2
Andrew Cutler <macropin@gmail.com>
2012-10-14 22:13:57 +1100

    Initial prototype

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,3 +1,71 @@
 #!/bin/sh
 
 # Adlibre Backup - Backup Runner
+
+CWD="$(dirname $0)/"
+
+# Source Config
+. ${CWD}../etc/backup.conf
+
+# Source Functions
+. ${CWD}functions.sh;
+
+HOST=$1
+HOSTS_DIR="/${ZPOOL_NAME}/hosts/"
+LOCKFILE="/var/run/$(basename $0 | sed s/\.sh//)-${HOST}.pid"
+LOGFILE="${HOSTS_DIR}${HOST}/l/backup.log"
+
+if [ ! $(whoami) = "andrew" ]; then
+    echo "Error: Must run as root."
+    exit 99
+fi
+
+if [ ! ${HOST} ]; then
+	echo "Please specify host name as the first argument."
+	exit 99
+fi
+
+# Check to see if we are already running / locked, limit to one instance per host
+if [ -f ${LOCKFILE} ] ; then
+    logMessage 3 $LOGFILE "Error: Already running, or locked. Lockfile exists [$(ls -ld $LOCKFILE)]."
+    exit 99
+else
+    echo $$ > ${LOCKFILE}
+    # Upon exit, remove lockfile.
+    trap "{ rm -f ${LOCKFILE}; }" EXIT
+fi
+
+# source host config
+. "${HOSTS_DIR}${HOST}/c/backup.conf"
+
+for e in $EXCLUDE $EXCLUDE_ADDITIONAL; do
+    RSYNC_EXCLUDES="$RSYNC_EXCLUDES --exclude=${e}"
+done
+
+# Do backup
+RSYNC_CMD="rsync ${RSYNC_ARGS} ${RSYNC_ADDITIONAL_ARGS} ${RSYNC_EXCLUDES} ${SSH_USER}@${HOST}:'$BACKUP_PATHS' ${HOSTS_DIR}${HOST}/d/"
+logMessage 1 $LOGFILE "Running: $RSYNC_CMD"
+$RSYNC_CMD
+
+if [ "$?" = "0" ]; then
+    raiseAlert "backup ${HOST}" 0 "Backup successful"
+    logMessage 1 $LOGFILE "Backup Succeeded"
+else
+    raiseAlert "backup ${HOST}" 2 "Backup failed"
+    logMessage 3 $LOGFILE "Backup Failed"
+    exit 99
+fi
+
+# Create snapshot
+SNAP_NAME="${HOST}@$(date +"%F-%X-%s")"
+zfs snapshot $ZPOOL_NAME/hosts/${SNAP_NAME}
+if [ "$?" = "0" ]; then
+    raiseAlert "backup ${HOST}" 0 "Snapshot successful"
+    logMessage 1 $LOGFILE "Snapshot $SNAP_NAME created"
+else
+    raiseAlert "backup ${HOST}" 2 "Snapshot failed"
+    logMessage 3 $LOGFILE "Snapshot $SNAP_NAME Failed"
+    exit 99
+fi
+
+exit 0
---
+++ bin/functions.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#
+# Log Message to text file.
+#
+# Where Error Level is:
+# 0=debug
+# 1=info
+# 2=warn
+# 3=error
+#
+function logMessage {
+    # $1 = Error level
+    # $2 = logpath
+    # $3 = message 
+    DATE=$(date +"%F %X")
+    if [ "$1" -ge "$LOG_LEVEL" ]; then
+        echo "$DATE $3" >> ${2}
+        if [ "$ECHO_LOG" -eq "1" ]; then
+            echo $3
+        fi
+    fi
+}
+
+#
+# Send passive alert information to Nagios / Icinga
+#
+function raiseAlert {
+    # $1 - Service name that has been set up on nagios/nagiosdev server
+    # $2 - Return code 0=success, 1=warning, 2=critical
+    # $3 - Message you want to send
+    # <host_name>,<svc_description>,<return_code>,<plugin_output>
+    # defaults that can be overridden
+    NAGIOS_DIR=${NAGIOS_DIR-/usr/sbin/}
+    NAGIOS_CFG=${NAGIOS_CFG-/etc/nagios/}
+    NAGIOS_PORT=${NAGIOS_PORT-5667}
+    if [ -f ${NAGIOS_DIR}send_nsca ]; then
+        echo "`hostname`,$1,$2,$3" | ${NAGIOS_DIR}send_nsca -H ${NAGIOS_SERVER} \
+        -p ${NAGIOS_PORT} -d "," -c ${NAGIOS_CFG}send_nsca.cfg > /dev/null;
+        echo "Debug: Message Sent to Nagios ($NAGIOS_SERVER): $1 $2 $3.";
+    else
+        echo "Warning: NSCA (Nagios) Plugin not found.";
+    fi
+}
