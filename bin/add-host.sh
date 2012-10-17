4c54034e3a07b048d57d88f2e6fb7828c38c2e5d
Andrew Cutler <andrew@adlibre.com.au>
2012-10-17 11:56:30 +1100

    Wrong variable name

--- bin/add-host.sh
+++ bin/add-host.sh
@@ -11,15 +11,14 @@ CWD="$(dirname $0)/"
 . ${CWD}functions.sh;
 
 HOST=$1
-LOGFILE="${HOSTS_DIR}${HOST}/l/backup.log"
 
 if [ ! $HOST ]; then
 	echo "Please specify host name as the first argument."
 	exit
 fi
 
-zfs create ${ZPOOL_NAME}/hosts/${HOSTNAME}
-mkdir /${ZPOOL_NAME}/hosts/${HOSTNAME}/c
-mkdir /${ZPOOL_NAME}/hosts/${HOSTNAME}/d
-mkdir /${ZPOOL_NAME}/hosts/${HOSTNAME}/l
-cp /${ZPOOL_NAME}/etc/host_default.conf /${ZPOOL_NAME}/hosts/vz01.in.adlibre.net/c/backup.conf
+zfs create ${ZPOOL_NAME}/hosts/${HOST}
+mkdir /${ZPOOL_NAME}/hosts/${HOST}/c
+mkdir /${ZPOOL_NAME}/hosts/${HOST}/d
+mkdir /${ZPOOL_NAME}/hosts/${HOST}/l
+cp /${ZPOOL_NAME}/etc/host_default.conf /${ZPOOL_NAME}/hosts/${HOST}/c/backup.conf
