2c4daa3a411fcc250624cc5e1aae7f0fee77bfc9
Andrew Cutler <macropin@gmail.com>
2013-09-19 13:30:05 +1000

    Initial Add of NSCA testing script

---
+++ bin/test-alerts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Adlibre Backup - Test NSCA Alerts
+
+CWD="$(dirname $0)/"
+
+# Source Config
+. ${CWD}../etc/backup.conf
+
+# Source Functions
+. ${CWD}functions.sh;
+
+HOST=$1
+ANNOTATION=${2-none}
+
+if [ ! ${HOST} ]; then
+    echo "Usage: test-alerts.sh <hostname> <annotation>."
+    exit 99
+fi
+
+raiseAlert "${ANNOTATION}" 0 "Test Message" ${HOST}
